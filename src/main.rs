600bc1a3bc76b6b3560d6b388d07f8702ecfd308
Gabriel Saiago <grsaiago@gmail.com>
2025-09-23 20:12:09 -0300

    chore: read csv

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use polars::prelude::*;
+use std::path::PathBuf;
+
 fn main() {
-    println!("Hello, world!");
+    let df = CsvReadOptions::default()
+        .with_has_header(true)
+        .try_into_reader_with_file_path(Some(PathBuf::from(concat!(
+            env!("CARGO_MANIFEST_DIR"),
+            "/",
+            "dataset.csv"
+        ))))
+        .unwrap()
+        .finish();
+    println!("{:?}", df);
 }
