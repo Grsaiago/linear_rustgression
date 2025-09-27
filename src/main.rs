432d5f613559cd29b69f4639ba6481c092b38bf3
Gabriel Saiago <grsaiago@gmail.com>
2025-09-27 19:46:42 -0300

    chore: z-normalization working with tests

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use polars::prelude::*;
 use std::path::PathBuf;
 
 fn main() {
-    let df = CsvReadOptions::default()
+    let lf = CsvReadOptions::default()
         .with_has_header(true)
         .try_into_reader_with_file_path(Some(PathBuf::from(concat!(
             env!("CARGO_MANIFEST_DIR"),
@@ -11,55 +11,77 @@ fn main() {
         ))))
         .unwrap()
         .finish()
-        .unwrap();
-
-    let km_series = df.column("km").unwrap().as_series().unwrap();
-    let price_series = df.column("price").unwrap().as_series().unwrap();
-
-    let km_mean = km_series.mean().unwrap();
-    let km_stddev = km_series.std(1).unwrap();
-
-    let price_mean = price_series.mean().unwrap();
-    let price_stddev = price_series.std(1).unwrap();
-
-    println!("km_mean: {km_mean:?}");
-    println!("km_stddv: {km_stddev:?}");
-
-    println!("price_mean: {price_mean:?}");
-    println!("price_stddv: {price_stddev:?}");
-    println!("{df:?}");
-
-    let normalized_df = df!(
-        "km" => km_series.i64().unwrap().iter().map(|val| {
-            (val.unwrap() as f64 * km_mean) / km_stddev
-    }).collect::<Vec<_>>(),
-        "price" => price_series.i64().unwrap().iter().map(|val| {
-            (val.unwrap() as f64 * price_mean) / price_stddev
-        }).collect::<Vec<_>>()
-    );
-    println!("{normalized_df:?}");
+        .unwrap()
+        .lazy();
 }
 
-fn get_z_normalized_series(
-    series: &polars::series::Series,
-    datatype: polars::datatypes::DataType,
-) -> impl Into<Series> {
-    let stddev: f64 = series.std_reduce(1).unwrap().as_any_value().strict_cast
-    let mean = series.mean().unwrap();
+fn z_normalize_dataframe(lf: &LazyFrame) -> DataFrame {
+    let mut lf = lf.clone();
 
-    match datatype {
-        DataType::Int64 => series
-            .i64()
+    let lf_schema = lf.collect_schema().unwrap();
+
+    lf.select(
+        lf_schema
+            .iter_names()
+            .map(|column_name| {
+                ((col(column_name.clone()) - col(column_name.clone()).mean())
+                    / col(column_name.clone()).std(1))
+                .alias(format!("z_{column_name}"))
+            })
+            .collect::<Vec<_>>(),
+    )
+    .collect()
+    .unwrap()
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_z_normalize_dataframe() {
+        let lf = CsvReadOptions::default()
+            .with_has_header(true)
+            .try_into_reader_with_file_path(Some(PathBuf::from(concat!(
+                env!("CARGO_MANIFEST_DIR"),
+                "/",
+                "dataset.csv"
+            ))))
             .unwrap()
-            .iter()
-            .map(|val| (val.unwrap() as f64 * mean) / stddev)
-            .collect::<Series>(),
-        DataType::Float64 => series
-            .f64()
+            .finish()
             .unwrap()
-            .iter()
-            .map(|val| (val.unwrap() as f64 * mean) / stddev)
-            .collect::<Series>(),
-        _ => panic!("unsuported value type"),
+            .lazy();
+        let normalized_df = z_normalize_dataframe(&lf);
+        let check = normalized_df
+            .clone()
+            .lazy()
+            .select([
+                col("z_price")
+                    .mean()
+                    .round(2, RoundMode::HalfAwayFromZero)
+                    .alias("mean_price"),
+                col("z_price")
+                    .std(1)
+                    .alias("std_price")
+                    .round(2, RoundMode::HalfAwayFromZero),
+                col("z_km")
+                    .mean()
+                    .round(2, RoundMode::HalfAwayFromZero)
+                    .alias("mean_km"),
+                col("z_km")
+                    .std(1)
+                    .round(2, RoundMode::HalfAwayFromZero)
+                    .alias("std_km"),
+            ])
+            .collect()
+            .unwrap();
+        let expected = df![
+            "mean_price" => [0.0],
+            "std_price" => [1.0],
+            "mean_km" => [0.0],
+            "std_km" => [1.0],
+        ]
+        .unwrap();
+        assert!(check.equals(&expected));
     }
 }
